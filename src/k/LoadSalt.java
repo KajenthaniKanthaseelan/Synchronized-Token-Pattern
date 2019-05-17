90fd38da13fe0a5c92792ece9a3e03378e9881ab
Kajenthani.K <kajeekanthan@gmail.com>
2019-05-17 21:35:41 +0530

    Project Provided

---
+++ src/k/LoadSalt.java
@@ -0,0 +1,14 @@
+package k;
+
+import com.sun.org.apache.xalan.internal.xsltc.dom.Filter;
+
+
+public class LoadSalt implements Filter{
+
+	@Override
+	public boolean test(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
